a038a60ffd8274dee1f70dffdb4b8e1a595ae102
Pwea <98360627+Pwea@users.noreply.github.com>
2022-09-29 17:35:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -164,7 +164,7 @@ class Game_Over {
         context.fillText(`Score: ${this.score} points`,this.x + 50,this.y + 50)
         context.fillText(`Time: ${this.time} seconds`, this.x + 50, this.y + 80)
         context.font = "15px Georgia, serif";
-        context.fillText("Made By @Pwea", this.btnX + 300, this.btnY + 100)
+        context.fillText("Made By @Pwea", this.btnX + 250, this.btnY + 100)
         context.fillStyle = "white"
         context.font = "20px Georgia, serif";
         context.fillText("Play Again", this.btnX + 120, this.btnY + 25)
